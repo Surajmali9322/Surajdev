26f19cb2021e173521939e63e894fb3cae48aff8
Suraj Mali <surajmali9322@gmail.com>
2025-09-02 17:03:25 +0530

    Add files via upload

---
+++ Assignment 1/program4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>  
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    
+    ch = tolower(ch);
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        printf("%c is a Vowel.\n", ch);
+    } else if ((ch >= 'a' && ch <= 'z')) {
+        printf("%c is a Consonant.\n", ch);
+    } else {
+        printf("Invalid input! Please enter an alphabet.\n");
+    }
+
+    return 0;
+}
+
